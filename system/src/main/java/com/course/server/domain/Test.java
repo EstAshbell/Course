ef8e1624f9c3d91b701ea78c613cf3392287ef4f
Ashbell <16838220@qq.com>
2020-09-21 21:47:01 +0800

    新建公共模块server

---
+++ system/src/main/java/com/course/server/domain/Test.java
@@ -0,0 +1,27 @@
+package com.course.server.domain;
+
+/**
+ * @author: xyl
+ * @time: 2020/9/21 19:26
+ * @description:
+ */
+public class Test {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
