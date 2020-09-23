56d8d4c2e9428e4ff3f46c249bee7d66a35a4635
Ashbell <16838220@qq.com>
2020-09-23 14:00:30 +0800

    创建business模块

---
+++ business/src/main/java/com/course/business/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.course.business.controller;
+
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author: xyl
+ * @time: 2020/9/21 11:14
+ * @description:
+ */
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("test")
+    public List<Test> test1(){
+        return testService.test();
+    }
+}
