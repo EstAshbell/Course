82de9cc5bafd1b9b34d1161827bae2c30c887805
Ashbell <16838220@qq.com>
2020-10-20 16:26:38 +0800

    添加toast.js通用提示文件，内部使用sweatalert2实现

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -102,14 +102,31 @@
         methods:{
             del(id){
                 let _this = this;
-                _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/"+id,_this.chapter).then((response)=>{
-                    console.log("删除大章结果：",response);
-                    var resp = response.data;
-                    if (resp.success){
-                        $("#form-modal").modal("hide");
-                        _this.list(1);
+
+                Swal.fire({
+                    title: '确认删除？',
+                    text: "删除后不可恢复，确认删除？",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: '删除',
+                    cancelButtonText :'取消'
+                }).then((result) => {
+                    if (result.value) {
+                        _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/"+id,_this.chapter).then((response)=>{
+                            console.log("删除大章结果：",response);
+                            var resp = response.data;
+                            if (resp.success){
+                                _this.list(1);
+                                toast.success("删除成功！");
+                            }
+                        })
+
                     }
                 })
+
+
             },
             save(){
                 let _this = this;
