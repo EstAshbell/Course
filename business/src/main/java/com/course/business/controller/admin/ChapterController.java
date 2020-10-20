ce1065d9f7021760f2b4d6a170701d58cd5aa8fb
Ashbell <16838220@qq.com>
2020-10-20 21:01:27 +0800

    增加AOP统一日志输出 logback增加日志跟踪号

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -7,6 +7,8 @@ import com.course.server.dto.ResponseDto;
 import com.course.server.service.ChapterService;
 import com.course.server.util.ValidatorUtil;
 import org.apache.ibatis.annotations.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -21,11 +23,16 @@ import java.util.List;
 @RequestMapping("/admin/chapter")
 public class ChapterController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+    public static final String BUSINESS_NAME = "大章";
+
     @Resource
     private ChapterService chapterService;
 
     @RequestMapping("/list")
     public ResponseDto list(@RequestBody PageDto pageDto) {
+        LOG.info("pageDto: {}",pageDto);
+
         ResponseDto<Object> responseDto = new ResponseDto<>();
         chapterService.list(pageDto);
         responseDto.setContent(pageDto);
@@ -33,6 +40,8 @@ public class ChapterController {
     }
     @RequestMapping("/save")
     public ResponseDto save(@RequestBody ChapterDto chapterDto) {
+        LOG.info("chapterDto: {}",chapterDto);
+
         ValidatorUtil.require(chapterDto.getName(), "名称");
         ValidatorUtil.require(chapterDto.getCourseId(), "课程ID");
         ValidatorUtil.length(chapterDto.getCourseId(), "名称",1,8);
@@ -44,6 +53,8 @@ public class ChapterController {
     }
     @DeleteMapping("/delete/{id}")
     public ResponseDto save(@PathVariable("id") String id) {
+        LOG.info("id: {}", id);
+
         ResponseDto<Object> responseDto = new ResponseDto<>();
         chapterService.delete(id);
         return responseDto;
