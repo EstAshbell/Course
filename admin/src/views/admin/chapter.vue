01046d59875a7b6228b0cb41896ffe630d7f6d60
Ashbell <16838220@qq.com>
2020-10-21 21:31:09 +0800

    增加vue多环境配置，后端地址改为读环境变量

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -108,7 +108,7 @@
 
                 Confirm.show("删除大章后不可恢复，确认删除？",function (result) {
                     Loading.show();
-                    _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/"+id,_this.chapter).then((response)=>{
+                    _this.$ajax.delete(process.env.VUE_APP_SERVER+"/business/admin/chapter/delete/"+id,_this.chapter).then((response)=>{
                         Loading.hide();
                         var resp = response.data;
                         if (resp.success){
@@ -132,7 +132,7 @@
 
 
                 Loading.show();
-                _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/save",_this.chapter).then((response)=>{
+                _this.$ajax.post(process.env.VUE_APP_SERVER+"/business/admin/chapter/save",_this.chapter).then((response)=>{
                     Loading.hide();
                     var resp = response.data;
                     if (resp.success){
@@ -167,7 +167,7 @@
             list(page){
                 let _this = this;
                 Loading.show();
-                _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/list",{
+                _this.$ajax.post(process.env.VUE_APP_SERVER+"/business/admin/chapter/list",{
                     page:page,
                     size:_this.$refs.pagination.size
                 }).then((response)=>{
