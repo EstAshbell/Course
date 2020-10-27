8c3aa74d328e18de7a6164ece1b86a027e0960f5
Ashbell <16838220@qq.com>
2020-10-27 15:15:34 +0800

    大章管理与小节管理互相跳转 小节管理保存和查询功能关联courseId, chapterId

--- business/src/main/java/com/course/business/controller/admin/SectionController.java
+++ business/src/main/java/com/course/business/controller/admin/SectionController.java
@@ -1,8 +1,8 @@
 package com.course.business.controller.admin;
 
-import com.course.server.dto.SectionDto;
-import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
+import com.course.server.dto.SectionDto;
+import com.course.server.dto.SectionPageDto;
 import com.course.server.service.SectionService;
 import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author: xyl
@@ -31,10 +30,12 @@ public class SectionController {
      * @title : 查询
      */
     @RequestMapping("/list")
-    public ResponseDto list(@RequestBody PageDto pageDto) {
+    public ResponseDto list(@RequestBody SectionPageDto sectionPageDto) {
         ResponseDto<Object> responseDto = new ResponseDto<>();
-        sectionService.list(pageDto);
-        responseDto.setContent(pageDto);
+        ValidatorUtil.require(sectionPageDto.getCourseId(), "课程ID");
+        ValidatorUtil.require(sectionPageDto.getChapterId(), "大章ID");
+        sectionService.list(sectionPageDto);
+        responseDto.setContent(sectionPageDto);
         return responseDto;
     }
     /*
