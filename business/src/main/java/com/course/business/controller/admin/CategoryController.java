58a0dffadccdcd5fc06f77998cdb2038cd746486
Ashbell <16838220@qq.com>
2020-11-03 16:31:36 +0800

    分类列表查询与显示，左右两个表格

--- business/src/main/java/com/course/business/controller/admin/CategoryController.java
+++ business/src/main/java/com/course/business/controller/admin/CategoryController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: xyl
@@ -25,6 +26,17 @@ public class CategoryController {
 
     @Resource
     private CategoryService categoryService;
+
+    /*
+     * @title : 查询不分页
+     */
+    @RequestMapping("/all")
+    public ResponseDto all() {
+        ResponseDto<Object> responseDto = new ResponseDto<>();
+        List<CategoryDto> list = categoryService.all();
+        responseDto.setContent(list);
+        return responseDto;
+    }
 
     /*
      * @title : 查询
