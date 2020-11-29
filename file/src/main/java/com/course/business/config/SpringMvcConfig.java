6829121a5c909f54061d6a04437c571eeeccbae0
Ashbell <16838220@qq.com>
2020-11-29 22:16:46 +0800

    文件上传功能开发：讲师头像显示优化

--- file/src/main/java/com/course/business/config/SpringMvcConfig.java
+++ file/src/main/java/com/course/business/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.course.business.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,8 +12,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:E:/course/file/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:"+FILE_PATH);
     }
 }
