d129d82d3e56baa8fa8bd7ddd0b0a287579e9e58
Ashbell <16838220@qq.com>
2020-12-03 00:37:59 +0800

    文件上传功能开发：将文件内容模态框做成单独的页面，代码更容易维护，内容编辑更方便

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,12 +1,13 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Login from "./views/login.vue"
-import Admin from "./views/admin"
-import Welcome from "./views/admin/welcome"
-import Chapter from "./views/admin/chapter"
-import Section from "./views/admin/section"
-import Course from "./views/admin/course"
-import Category from "./views/admin/category"
+import Admin from "./views/admin.vue"
+import Welcome from "./views/admin/welcome.vue"
+import Category from "./views/admin/category.vue"
+import Course from "./views/admin/course.vue"
+import Chapter from "./views/admin/chapter.vue"
+import Section from "./views/admin/section.vue"
+import Content from "./views/admin/content.vue"
 import Teacher from "./views/admin/teacher.vue"
 import File from "./views/admin/file.vue"
 
@@ -25,36 +26,38 @@ export default new Router({
         path: "/",
         name: "admin",
         component: Admin,
-        children:[{
-            path:"welcome", //子路由不能斜杠/开头
-            name:"welcome",
-            component:Welcome
-        },{
-            path:"business/course",
-            name:"business/course",
-            component:Course
-        },{
-            path:"business/chapter",
-            name:"business/chapter",
-            component:Chapter
-        },{
-            path:"business/section",
-            name:"business/section",
-            component:Section
-        },{
-            path:"business/category",
-            name:"business/category",
-            component:Category
-        },{
-            path:"business/teacher",
-            name:"business/teacher",
-            component:Teacher
-
-        },{
-            path:"file/file",
-            name:"file/file",
-            component:File
-
+        children: [{
+            path: "welcome",
+            name: "welcome",
+            component: Welcome,
+        }, {
+            path: "business/category",
+            name: "business/category",
+            component: Category,
+        }, {
+            path: "business/course",
+            name: "business/course",
+            component: Course,
+        }, {
+            path: "business/chapter",
+            name: "business/chapter",
+            component: Chapter,
+        }, {
+            path: "business/section",
+            name: "business/section",
+            component: Section,
+        }, {
+            path: "business/content",
+            name: "business/content",
+            component: Content,
+        }, {
+            path: "business/teacher",
+            name: "business/teacher",
+            component: Teacher,
+        }, {
+            path: "file/file",
+            name: "file/file",
+            component: File,
         }]
     }]
 })
